0cde8cf43bdb534e80956db063cf2ce8b1aafcbf
unknown <e.fraffo@gmail.com>
2020-04-19 20:15:07 -0300

    19/04

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import Hero from "./components/Hero";
+import Filtros from "./components/Filtros";
+import Hoteles from "./components/Hoteles";
+import Moment from "moment";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      filters: {
+        dateFrom: Moment(new Date()).format("YYYY-MM-DD"),
+        dateTo: Moment()
+          .add(1, "month")
+          .format("YYYY-MM-DD"),
+        country: "select",
+        price: "select",
+        rooms: "select"
+      },
+      hotels: [],
+      filteredHotels: [],
+      isAllLoaded: false
+    };
+  }
+
+  render(){
+    return(
+      <Fragment>
+         <Hero filters={this.state.filters}/>
+         <Filtros />
+         <Hoteles />
+      </Fragment>  
+    );  
+  }
 }
+
 
 export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+
+class Hero extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {};
+    }
+
+    render(){
+        return(
+            <section className="hero is-primary">
+                <div className="hero-body">
+                    <div className="container">
+                    <h1 className="title">Hoteles</h1>
+                    <h2 className="subtitle">
+                        desde el <strong>dddd, DD de mmmm de AAAA</strong> hasta el <strong>dddd, DD de mmmm de AAAA</strong>
+                    </h2>
+                    </div>
+                </div>
+            </section>
+        );
+    }
+}
+
+export default Hero;
