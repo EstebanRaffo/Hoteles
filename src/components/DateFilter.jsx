45c67aa0e97fbb33e95189b8342b341cc7a4a08d
unknown <e.fraffo@gmail.com>
2020-04-29 20:24:33 -0300

    29/04

--- src/components/DateFilter.jsx
+++ src/components/DateFilter.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import 'bulma/css/bulma.css';
 
 class DateFilter extends Component{
     constructor(props){
@@ -16,10 +17,8 @@ class DateFilter extends Component{
                 <div className="control has-icons-left">
                     <input className="input" type="date" onChange={this.handleDateChange} value={this.props.date} name={this.props.name}/>
                     <span className="icon is-small is-left">
-                    <i className={`fas ${this.props.icon}`}></i>
-                        {/* <i className="fas" value={this.props.icon}></i> */}
-                        {/* <i className={this.props.icon}></i> */}
-                        {/* <ion-icon name="log-in-outline"></ion-icon> */}
+                        <i className={`fas ${this.props.icon}`}></i>
+                        
                         {/* Para mostrar el ícono la solución es simple: con que reciba como parámetro la cadena de texto fa-sign-in-alt para el 
                         caso de la fecha de ingreso y fa-sign-out-alt para la fecha de egreso, ya podríamos concatenar al elemento <i className="fas"></i>
                          dicho valor en el atributo className. */}
--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import DateFilter from "./DateFilter";
 import OptionsFilter from "./OptionsFilter";
-// import { select } from 'react-bulma';
+
 
 class Filters extends Component{
     constructor(props){
@@ -13,13 +13,11 @@ class Filters extends Component{
             precios:'',
             estilosNavVar: {
                 backgroundColor: "#0779e4",
-                padding: "15px",
                 margin: "0px 20px",
                 justifyContent: 'center'    
             },
             estilosItem: {
-                display: "inline-block",
-                margin: "0px 10px"
+                display: "inline-block"
             }
         };
     }
@@ -49,26 +47,31 @@ class Filters extends Component{
     // 1.4  Guía: Filtros (Parte 2) Checkpoint
     render(){
         return(
+            // style={this.state.estilosNavVar}
             <nav className="navbar is-info" style={this.state.estilosNavVar}>
+                {/* style={this.state.estilosItem}  */}
                 <div className="navbar-item" style={this.state.estilosItem}>
                     <DateFilter
                         date={ this.props.filters.dateFrom}
                         icon="sign-in-alt" 
                         onDateChange = {this.onDateChange}/>
                 </div>
+                {/* style={this.state.estilosItem} */}
                 <div className="navbar-item" style={this.state.estilosItem}>
                     <DateFilter
                         date={ this.props.filters.dateTo }
                         icon="sign-out-alt" 
                         onDateChange = {this.onDateChange}/>
                 </div>
+                {/* style={this.state.estilosItem} */}
                 <div className="navbar-item" style={this.state.estilosItem}>
                     <OptionsFilter
                         options = { [ {value: '', name: 'Todos los países'}, {value: 'Argentina', name: 'Argentina'}, {value: 'Brasil', name: 'Brasil'}, {value: 'Chile', name: 'Chile'}, {value: 'Uruguay', name: 'Uruguay'} ] } 
                         selected = { this.props.filters.country }
-                        icon = "globe" 
+                        icon = "fa-globe" 
                         onOptionChange = {this.onOptionChange}/>
                 </div>
+                {/* style={this.state.estilosItem} */}
                 <div className="navbar-item" style={this.state.estilosItem}>
                     <OptionsFilter
                         options = { [ {value: undefined, name: 'Cualquier precio'}, {value: 1, name: '$'}, {value: 2, name: '$$'}, {value: 3, name: '$$$'}, {value: 4, name: '$$$$'} ] }
@@ -76,6 +79,7 @@ class Filters extends Component{
                         icon = "dollar-sign" 
                         onOptionChange = {this.onOptionChange}/>
                 </div>
+                {/* style={this.state.estilosItem} */}
                 <div className="navbar-item" style={this.state.estilosItem}>
                     <OptionsFilter
                         options = { [ {value: undefined, name: 'Cualquier tamaño'}, {value: 10, name: 'Hotel pequeño'}, {value: 20, name: 'Hotel mediano'}, {value: 30, name: 'Hotel grande'} ] }
