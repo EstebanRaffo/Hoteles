92e49ddb1a8bdf4ea0a2b5c0f27f662e880ce1db
Esteban Raffo <e.fraffo@gmail.com>
2020-08-01 19:08:23 -0300

    Updates

---
+++ precache-manifest.6a8df964d82a3155b336766e0ebf443c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1baa6dc3f6f2dcd92f4e649024c167b4",
+    "url": "/Hoteles/index.html"
+  },
+  {
+    "revision": "9b5a7fdb75a60933f9e2",
+    "url": "/Hoteles/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "556004f2ec83a420831a",
+    "url": "/Hoteles/static/js/2.407ee670.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Hoteles/static/js/2.407ee670.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b5a7fdb75a60933f9e2",
+    "url": "/Hoteles/static/js/main.5a30dc8a.chunk.js"
+  },
+  {
+    "revision": "0c21bc38dbde2397df82",
+    "url": "/Hoteles/static/js/runtime-main.f7f4306c.js"
+  }
+]);
